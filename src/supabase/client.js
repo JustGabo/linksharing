28d89e3bb8b52aace62406a3a2770031fa77e9f8
Gabriel Soriano <ovallegabriel123@gmail.com>
2023-09-05 16:02:01 -0400

    my fisrt commit

---
+++ src/supabase/client.js
@@ -0,0 +1,8 @@
+import {createClient} from '@supabase/supabase-js'
+
+const URL = 'https://yyknfyrwtbhttcrroxdm.supabase.co'
+const anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5a25meXJ3dGJodHRjcnJveGRtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM5NDE5NjIsImV4cCI6MjAwOTUxNzk2Mn0.f5971jPMuZScNaQS58DhiHdrVMu6-chXPWHrnY5bVYE'
+
+const supabase = createClient(URL, anonKey)
+
+export default supabase
