44ddb97855018ccecbc319640c467627f8811770
stylessh <stylesshjs@gmail.com>
2023-09-23 14:40:47 -0400

    feat: adding images to the landing page

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 
 export const footer = () => {
   return (
-    <div className="absolute bottom-0 px-10 w-[100%] right-0 left-0 m-auto text-xs border-gray-300 font-medium py-4 border-t">
+    <div className="fixed bottom-0 px-10 w-[100%] inset-x-0 bg-white m-auto text-xs border-gray-300 font-medium py-4 border-t">
         <p>Gabriel Soriano 2023. All rights reserved</p>
     </div>
   )
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 // import { PlusCircle} from "lucide-react";
 import {footer as Footer} from '../components/footer'
+import CustomizeImage from '../images/image.png'
+// import ProfileImage from '../images/image3.png'
+import PreviewImage from '../images/image4.png'
 
 export const Landing = () => {
   return (
     <div className="mt-8 w-[95%] m-auto">
-      <header className="flex items-center justify-between mb-44">
+      <header className="flex items-center justify-between mb-4">
         <div>
           <h1 className="text-2xl font-bold md:text-3xl">Link</h1>
         </div>
@@ -28,12 +31,17 @@ export const Landing = () => {
 
       
 
-      <main className="grid grid-cols-2">
+      <main className="grid items-center grid-cols-[55%,1fr]">
         <div className="flex flex-col gap-5">
           <h2 className="text-5xl  md:text-7xl font-semibold text-purple-500 w-[100%] md:w-[100%]">
             Create your own <span className="text-black">linktree</span>
           </h2>
-          <p className="md:text-sm text-xs text-black w-[90%] md:w-[100%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vel in eligendi voluptatem quaerat accusamus pariatur reprehenderit dicta, nihil vitae dignissimos odit quia eos perspiciatis id molestiae? Iusto, quo officia.</p>
+          <p className="md:text-sm text-xs text-black w-[90%] md:w-[90%]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est vel in eligendi voluptatem quaerat accusamus pariatur reprehenderit dicta, nihil vitae dignissimos odit quia eos perspiciatis id molestiae? Iusto, quo officia.</p>
+        </div>
+        <div className="grid gap-2 p-2 ">
+        <img className="w-[94%] ml-auto" src={CustomizeImage} alt="" />
+        <img className="w-[94%]" src={PreviewImage} alt="" />
+        {/* <img className="w-[60%]" src={ProfileImage} alt="" /> */}
         </div>
       </main>
 
